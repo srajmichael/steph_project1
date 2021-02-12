1fa1065ba472f9e1b5834f680b45bdd809ccfbec
Mike Sraj <michaelsrajjr@gmail.com>
2021-02-12 11:16:48 -0500

    Initalize

---
+++ images.js
@@ -0,0 +1,38 @@
+const testImages = [
+   {
+      src:'https://images.ctfassets.net/hrltx12pl8hq/1zlEl4XHkxeDuukJUJyQ7Y/a149a908727e2084d503dc103a620d7f/lohp-image-img-3.jpg',
+      title: 'New Image 1'
+   },
+   {
+      src:'https://dynaimage.cdn.cnn.com/cnn/c_fill,g_auto,w_1200,h_675,ar_16:9/https%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F191120053137-03-milky-way-images-australia.jpg',
+      title: 'New Image 2'
+   },
+   {
+      src:'https://www.wallpapertip.com/wmimgs/152-1522540_good-night-jesus-images-inner-child.jpg',
+      title: 'Additional Image 3'
+   },
+   {
+      src:'https://cdn.pixabay.com/photo/2015/02/24/15/41/dog-647528__340.jpg',
+      title: 'Next Image 4'
+   },
+   {
+      src:'https://www.onceuponapicture.co.uk/wp-content/uploads/2019/02/46456227_2504829799542273_7554593422053474304_o-700x525.jpg',
+      title: 'Another Image 5'
+   },
+   {
+      src:'https://cdn.pixabay.com/photo/2017/11/07/00/07/fantasy-2925250_960_720.jpg',
+      title: 'New Image 8'
+   },
+   {
+      src:'https://www.ilounge.com/wp-content/uploads/2020/12/new-year-4656853_1280-1024x579.jpg',
+      title: 'Additional Image 7'
+   },
+   {
+      src:'https://iso.500px.com/wp-content/uploads/2014/02/35882320-2048.jpeg',
+      title: 'Next Image 8'
+   },
+   {
+      src:'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg',
+      title: 'Another Image 9'
+   },
+]
